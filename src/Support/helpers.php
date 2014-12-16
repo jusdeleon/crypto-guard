4ee3653c0dc29e33ac3b0788b3a4b816fca135b5
Chris Bautista <chrisbjr@gmail.com>
2014-12-16 22:52:21 +0800

    Removed test code - and ran some formatting

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -21,20 +21,14 @@ if ( ! function_exists('hash_pbkdf2')) {
     {
         // check for hashing algorithm
         if ( ! in_array(strtolower($algo), hash_algos())) {
-            trigger_error(sprintf(
-                '%s(): Unknown hashing algorithm: %s',
-                __FUNCTION__, $algo
-            ), E_USER_WARNING);
+            trigger_error(sprintf('%s(): Unknown hashing algorithm: %s', __FUNCTION__, $algo), E_USER_WARNING);
             return false;
         }
 
         // check for type of iterations and length
         foreach (array(4 => $iterations, 5 => $length) as $index => $value) {
             if ( ! is_numeric($value)) {
-                trigger_error(sprintf(
-                    '%s() expects parameter %d to be long, %s given',
-                    __FUNCTION__, $index, gettype($value)
-                ), E_USER_WARNING);
+                trigger_error(sprintf('%s() expects parameter %d to be long, %s given', __FUNCTION__, $index, gettype($value)), E_USER_WARNING);
                 return null;
             }
         }
@@ -42,29 +36,20 @@ if ( ! function_exists('hash_pbkdf2')) {
         // check iterations
         $iterations = (int) $iterations;
         if ($iterations <= 0) {
-            trigger_error(sprintf(
-                '%s(): Iterations must be a positive integer: %d',
-                __FUNCTION__, $iterations
-            ), E_USER_WARNING);
+            trigger_error(sprintf('%s(): Iterations must be a positive integer: %d', __FUNCTION__, $iterations), E_USER_WARNING);
             return false;
         }
 
         // check length
         $length = (int) $length;
         if ($length < 0) {
-            trigger_error(sprintf(
-                '%s(): Iterations must be greater than or equal to 0: %d',
-                __FUNCTION__, $length
-            ), E_USER_WARNING);
+            trigger_error(sprintf('%s(): Iterations must be greater than or equal to 0: %d', __FUNCTION__, $length), E_USER_WARNING);
             return false;
         }
 
         // check salt
         if (strlen($salt) > PHP_INT_MAX - 4) {
-            trigger_error(sprintf(
-                '%s(): Supplied salt is too long, max of INT_MAX - 4 bytes: %d supplied',
-                __FUNCTION__, strlen($salt)
-            ), E_USER_WARNING);
+            trigger_error(sprintf('%s(): Supplied salt is too long, max of INT_MAX - 4 bytes: %d supplied', __FUNCTION__, strlen($salt)), E_USER_WARNING);
             return false;
         }
 
@@ -95,19 +80,6 @@ if ( ! function_exists('hash_pbkdf2')) {
         }
 
         return $derivedKey;
-    }
-
-    // test
-    if (realpath($_SERVER['argv'][0]) === __FILE__ && function_exists('hash_pbkdf2')) {
-        $password = 'password';
-        $savedPassword = 'sha256,salt,65536,4156f668bb31db3a17f4d1b91424ef0d417ad1f35d055aceaebd8da0f6a44b7e';
-        list($algo, $salt, $iterations, $hash) = explode(',', $savedPassword);
-
-        var_dump(
-            $hash,
-            hash_pbkdf2($algo, $password, $salt, $iterations, strlen($hash)),
-            compat_pbkdf2($algo, $password, $salt, $iterations, strlen($hash))
-        );
     }
 
 }
