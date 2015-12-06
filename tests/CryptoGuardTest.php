5d60ca07d87a338b0408ae3eb310b67034e077be
Chris Bautista <chrisbjr@gmail.com>
2015-12-06 22:14:54 +0800

    Added a new test for tampering with decryption

--- tests/CryptoGuardTest.php
+++ tests/CryptoGuardTest.php
@@ -5,7 +5,7 @@ class CryptoGuardTest extends PHPUnit_Framework_TestCase
 
     public function testEncryption()
     {
-        require __DIR__ . '/../vendor/autoload.php';
+        require_once __DIR__ . '/../vendor/autoload.php';
 
         $passphrase = 'test-passphrase';
 
@@ -18,6 +18,32 @@ class CryptoGuardTest extends PHPUnit_Framework_TestCase
         $decryptedText = $cryptoGuard->decrypt($encryptedText);
 
         $this->assertEquals($stringToEncrypt, $decryptedText);
+    }
+
+    /**
+     * @expectedException Coreproc\CryptoGuard\Exceptions\InvalidPayloadException
+     */
+    public function testFailedDecryption()
+    {
+        require_once __DIR__ . '/../vendor/autoload.php';
+
+        $passphrase = 'test-passphrase';
+
+        $cryptoGuard = new \Coreproc\CryptoGuard\CryptoGuard($passphrase);
+
+        $stringToEncrypt = 'test string to encrypt';
+
+        $encryptedText = $cryptoGuard->encrypt($stringToEncrypt);
+
+        // decrypt the string
+        $decryptedText = json_decode(base64_decode($encryptedText), true);
+
+        $decryptedText['iv'] = 'changed-iv-for-test';
+
+        // encrypt again
+        $encryptedText = base64_encode(json_encode($decryptedText));
+
+        $decryptedText = $cryptoGuard->decrypt($encryptedText);
     }
 
 }
